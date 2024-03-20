82fd162f0cf7efdefac5c91111adfc58edecea37
raed <raed.da3k@gmail.com>
2024-03-20 11:10:58 +0100

    add

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
+// a script that display the status code of a GET request.
 const request = require('request');
-
-const url = process.arg[2];
-
-request(url, (error, response) => {
-  if (error) {
-    console.error(error);
+const url = process.argv[2];
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
   } else {
     console.log('code:', response.statusCode);
   }
